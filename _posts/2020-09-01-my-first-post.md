7069344502f80f096e1611d49d372434c9d332f3
Irene Kim <>
2020-09-01 16:20:22 +0900

    sample commit

---
+++ _posts/2020-09-01-my-first-post.md
@@ -0,0 +1,22 @@
+---
+title: Hello Jekyll!!
+tags:
+- wow
+- hey
+categories:
+- blog
+---
+
+## Why Jekyll?
+
+uhm...
+
+안녕하세요
+
+
+- 와우
+- 하하
+
+## 안녕앙ㄴ녕
+
+- ㅎㅎㅎㅎ
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Hello Jekyll!!",
+        "excerpt":"Why Jekyll?   uhm…   안녕하세요      와우   하하   안녕앙ㄴ녕      ㅎㅎㅎㅎ  ","categories": ["blog"],
+        "tags": ["wow","hey"],
+        "url": "http://localhost:4000/blog/my-first-post/",
+        "teaser": null
+      }]
